daf5afd0b6ecb1dc210e99a14b8608428bd839f3
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-06-07 20:41:14 +0545

    Some more updates

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Projects from './components/Projects/Projects'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+      <Footer />
+    </Router>
+          
     </>
   )
 }
--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import Santosh from "../../../public/images/santosh.png";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="main-container">
+        <div className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="info" style={{zIndex:'10'}}>
+          <div className="main-circle">
+            <img
+              src={Santosh}
+              alt=""
+              style={{ height: "800px" }}
+            />
+          </div>
+          <div className="description">
+            <h1
+              style={{
+                color: "#13ff00",
+                fontSize: "70px",
+                textTransform: "uppercase",
+                fontWeight: "bolder",
+              }}
+            >
+              Santosh Upadhyay
+            </h1>
+
+            <h2
+              className="about-me"
+              style={{ color: "white", fontSize: "68px", fontWeight: "bolder" }}
+            >
+              Front End Developer
+            </h2>
+            <p style={{ fontSize: "40px", color: "white" }}>
+              Computer Science Student
+            </p>
+            {/* <p>
+              Front-end developer focused on creating Web and Mobile
+              applications. I really like different projects that solve real
+              problems.
+            </p> */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
